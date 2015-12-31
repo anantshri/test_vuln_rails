8fc18fb220bd098a119134b8e5e391279ce688c4
Anant Shrivastava <anant@anantshri.info>
2016-01-01 03:32:38 +0530

    updated repo

--- checkinstall.sh
+++ checkinstall.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+echo "Setup Bundle files"
 bundle install --path vendor
-brakeman ./
-bundle-audit check --update
-dawn -r ./
+echo "Install and Run brakeman"
+gem install brakeman && brakeman ./
+echo "Install and Run bundler-audit and do a bundle audit"
+gem install bundler-audit && bundle-audit --update && bundle-audit check 
+echo "Install and run DawnScanner "
+gem install dawnscanner && dawn -r ./
+echo "Install and run retire.js"
+npm install retire && retire -j --path ./
+echo "All Test done"
